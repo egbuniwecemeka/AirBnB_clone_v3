8cb689c5b4628da4e1746188c44e10ed7f6427d3
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-02 10:46:05 -0700

    “”

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,6 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
+from subprocess import CalledProcessError
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
@@ -19,7 +20,8 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except (OSError, CalledProcessError) as e:
+        print(f'An error occurred: {e}')
         return None
 
 
@@ -40,7 +42,8 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except (OSError, CalledProccessError) as e:
+        print(f'An error occurred: {e}')
         return False
 
 
