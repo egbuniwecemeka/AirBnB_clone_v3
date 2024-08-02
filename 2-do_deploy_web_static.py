ad5615749da128823894417388e665c59a9a8cb9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-02 10:15:04 -0700

    pycodestyle test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
+from subprocess import CalledProcessError
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
@@ -26,5 +27,6 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except (OSError, Calledsubprocess) as e:
+        print(f'An error occures: {e}')
         return False
