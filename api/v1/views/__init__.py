7faec928f4875837f3498faa6b6408e6ec0a6754
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-05 15:17:09 -0700

    stats?

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,8 @@
 """ A Blueprint to a Flask instance """
 
 from flask import Blueprint
-from api.v1.views.index import get_status, objs_num
+# from api.v1.views.index import get_status, objs_num
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
-# from api.v1.views.index import *
+from api.v1.views.index import *
