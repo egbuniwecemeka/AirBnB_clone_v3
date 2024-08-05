5a77f673acd471732e243edbea739474c13ca75f
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-04 23:59:21 -0700

    pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,8 @@
 """ A Blueprint to a Flask instance """
 
 from flask import Blueprint
+# Import routes after defining blueprints after to avoid circular imports
+from api.v1.views.index import *
 
 # Blueprint instance with name and set url_prefix
 app_views = Blueprint("api_v1_bp", __name__, url_prefix="/api/v1")
-
-# Import routes after defining blueprints after to avoid circular imports
-from api.v1.views.index import * 
