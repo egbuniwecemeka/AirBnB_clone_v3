43f9eb0d6af79e30568df9daa3b29afaca5a2e60
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-05 06:06:57 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,11 @@
 """ Routing blueprint object to check app status """
 
 from flask import Flask, jsonify
-from . import app_views
+from api.v1.views import app_views
 
 
 # Routes to a flask blueprint
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ Returns json dict response of app status """
-    return jsonify(status='OK')
+    return jsonify({'status': 'OK'})
