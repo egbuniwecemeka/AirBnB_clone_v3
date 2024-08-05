3f4638f6b0d0649dc6a2ee7bf31adfb6d80ee4df
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-05 05:31:09 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 """ A flask application """
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
-# Create a Flask instance
+
 app = Flask(__name__)
 
-# Registering blueprint with flask application
-app.register_blueprint(app_views)
+
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def close_storage(exception):
+    """ close storage on error and after every request"""
     if exception:
         print(f'An error occurred: {exception}')
     storage.close()
 
 
+@app.errorhandler(404)
+def error404(err):
+    """ Handles 404 error """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', 5000)),
-            debug=True, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, debug=True, threaded=True)
