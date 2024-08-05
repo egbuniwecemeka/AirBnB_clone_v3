646d56050f446e2f954aaeb0d70f927da48f7d16
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-04 20:13:24 -0700

    Flask Blueprint for web application

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" A Blueprint to a Flask instance """ 
+
+from flask import Blueprint
+
+# Wild import of api.v1.views.index package (as required)
+from .index import *
+
+# Blueprint instance with name and set url_prefix
+app_views = Blueprint("api_v1_bp", __name__, url_prefix="/api/v1")
