083d31f1967ef0089d30f9fb655fc75555ac1c0e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-04 23:01:50 -0700

    app,py edit

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,18 +2,25 @@
 """ A flask application """
 
 from flask import Flask
+from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 # Create a Flask instance
 app = Flask(__name__)
 
+# Registering blueprint with flask application
+app.register_blueprint(app_views)
 
-def main_app():
-    """ Registering blueprint with flask application """
-    app.register_blueprint(app_views)
 
-    return app
+@app.teardown_appcontext
+def teardown(exception):
+    if exception:
+        print(f'An error occurred: {exception'}
+    storage.close()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', 5000)),
+            debug=True, threaded=True)
