47e86b4c5325ea2884e856af731b423b802189f6
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-05 15:55:42 -0700

    stats?

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,18 +5,14 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_storage(error):
     """ close storage on error and after every request"""
     if exception:
         print(f'An error occurred: {exception}')
@@ -24,12 +20,17 @@ def close_storage(exception):
 
 
 @app.errorhandler(404)
-def error404(err):
+def error404(error):
     """ Handles 404 error """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, debug=True, threaded=True)
+    """ Run only when executed as main function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
