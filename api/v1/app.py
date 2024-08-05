db2b7042ce157974f9f62605b2b0b18a548b60d9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-04 21:41:37 -0700

    flask updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ A flask application """
 
-from flask import Flask, jsonify
+from flask import Flask
+from api.v1.views import app_views
 
 # Create a Flask instance
 app = Flask(__name__)
 
+def main_app():
+    """ Registering blueprint with flask application """
+    app.register_blueprint(app_views)
 
-# Route to a URL that triggers a function
-@app.route('/api/v1/status', strict_slashes=False)
-def get_status():
-    return jsonify(status='OK')
+    return app
 
 
 if __name__ == "__main__":
---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" """
+
+from flask import Flask
+from . import app_views
+
+@app_views.route('views/status', ) 
