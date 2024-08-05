11978e09c7bc14d4d616c0c06b12428f4a2c8611
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-04 23:26:00 -0700

    circular import fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,5 @@
 
 from flask import Blueprint
 
-# Wild import of api.v1.views.index package (as required)
-from .index import *
-
 # Blueprint instance with name and set url_prefix
 app_views = Blueprint("api_v1_bp", __name__, url_prefix="/api/v1")
