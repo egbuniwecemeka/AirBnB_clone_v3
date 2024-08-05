e99b280c3e012ccb5996c166865bd7e5bbad9ead
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-05 06:53:09 -0700

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,15 @@
 
 """ Routing blueprint object to check app status """
 
-from flask import Flask, jsonify
+from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 # Routes to a flask blueprint
@@ -11,3 +18,17 @@ from api.v1.views import app_views
 def get_status():
     """ Returns json dict response of app status """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def objs_num():
+    """ Returns the number of objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
