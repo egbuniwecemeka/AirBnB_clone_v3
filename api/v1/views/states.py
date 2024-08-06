613e6f5cc55e817caf71c60dff3dad2944d2dd81
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-06 09:09:38 -0700

    Post/create state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,11 +3,11 @@
 
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, request, make_response
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def state_objs():
+def states_obj():
     """ Retrieves the list of all State objects """
 
     state_objs = storage.all(State).values()
@@ -43,3 +43,21 @@ def delete_state(state_id):
     storage.save()
 
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ Posts/creates a state  """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+        
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+
+    new_state = State(**data)
+    new_state.save()
+
+    return make_response(jsonify(new_state.to_dict()), 201)
+
