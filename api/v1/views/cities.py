1c73f4d188c504360509249ff765cb271939763a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-06 16:57:11 -0700

    Delete a City object

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,7 +5,7 @@ from models.city import City
 from models.state import State
 from api.v1.views import app_views
 from models import storage
-from flask import abort, jsonify
+from flask import abort, jsonify, make_response
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
@@ -33,3 +33,17 @@ def get_city(city_id):
         abort(404)
 
     return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """ Deletes a City object of city_id """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    storage.delete()
+    storage.save()
+
+    return make_response(jsonify({}), 200)
